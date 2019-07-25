62ee7c77cd2570b8d20472fcfddfd6da24697981
Doodle <deitelberg@gmail.com>
2019-07-24 22:16:53 -0400

    Creating stand-alone repo for hexagon script

---
+++ sample_config.py
@@ -0,0 +1,21 @@
+# SAMPLE CONFIG FILE FOR DEMONSTRATION PURPOSES
+# Config file to hold variables I don't want to put in the main file.
+# Also for later, so that I can put potentially sensitive info here
+# and not push it to GitHub accidentally. 
+
+adm1 = 'input/gadm36_USA_49.json'
+
+#epsg = 32662 #Kenya
+epsg = 5070 #USA
+
+seed_location = 'lowerleft'
+round_to = 10
+num_shape_divisor = 6
+
+symbology_column = 'County' #Kenya
+
+vertices_col = 'vertices'
+geom_col = 'geometry'
+
+tile_id = 'tile_id'
+admin_id = 'CID'
